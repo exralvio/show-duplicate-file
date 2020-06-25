e01d6ab89955decf411fbb972b009ed7cb93cc89
Alvio Leegia <exralvio@gmail.com>
2020-06-25 09:04:20 +0700

    Grouping duplicated files by size

--- duplicate.php
+++ duplicate.php
@@ -17,12 +17,12 @@ function fetchAllFiles($dir, &$results = []){
 
 function showDuplicate(){
     $filepaths = fetchAllFiles("./DropsuiteTest");
-    $ignore = ['.DS_Store'];
+    $ignored = ['.DS_Store'];
     $files = [];
 
     // wrap the details of files
     foreach($filepaths as $filepath){
-        if(filesize($filepath) == 0 || in_array(basename($filepath), $ignore)){
+        if(filesize($filepath) == 0 || in_array(basename($filepath), $ignored)){
             continue;
         }
 
@@ -33,7 +33,28 @@ function showDuplicate(){
         ];
     }
 
-    return $files;
+    // grouping duplicated files by size
+    $size_of_files = array_count_values(array_column($files, 'size'));
+    $duplicated_by_size = [];
+
+    foreach($size_of_files as $size => $count){
+        // ignore not duplicated
+        if($count <= 1){
+            continue;
+        }
+
+        // get all files with current size
+        $arr = array_filter($files, function($ar) use($size) {
+            return ($ar['size'] == $size);
+        });
+
+        // prevent double assign
+        if(!isset($duplicated_by_size[$size])){
+            $duplicate_by_size[$size] = $arr;
+        }
+    }
+
+    return $duplicate_by_size;
 }
 
 print_r(showDuplicate());

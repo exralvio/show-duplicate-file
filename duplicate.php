776a2c2eb364b9907fc58f8022260ceb7f33255b
Alvio Leegia <exralvio@gmail.com>
2020-06-25 08:32:00 +0700

    Fetch all files

--- duplicate.php
+++ duplicate.php
@@ -1,3 +1,18 @@
 <?php
 
-// show highest count of duplicated files
+function fetchAllFiles($dir, &$results = []){
+    $files = scandir($dir);
+
+    foreach($files as $key => $entry){
+        $path = realpath($dir . DIRECTORY_SEPARATOR . $entry);
+        if(!is_dir($path)){
+            $results[] = $path;
+        } else if ($entry != "." && $entry != ".."){
+            fetchAllFiles($path, $results);
+        }
+    }
+
+    return $results;
+}
+
+print_r(fetchAllFiles('./DropsuiteTest'));
